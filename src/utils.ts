2d1a9e56948ab88d0e6effd5940512d1c149d13a
Marck C. Guzman <marck@marck-devs.com>
2021-10-24 19:41:46 +0200

    Create formatter for strings

---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+export class Formatter {
+  /**
+   *
+   */
+  constructor() {}
+
+  public static formatObj(line: string, data: any): string {
+    let keys = Object.keys(data);
+    let out = line;
+    for (let k of keys) {
+      out = out.replace(new RegExp("\\{" + k + "\\}", "gi"), data[k]);
+    }
+    return out;
+  }
+  public static format(line: string, ...args): string {
+    let keys = Object.keys(args);
+    let out = line;
+    for (let k in keys) {
+      out = out.replace(new RegExp("\\{" + k + "\\}", "gi"), args[k]);
+    }
+    return out;
+  }
+}
