c3a8f875144acd344850a27d25b8bd380539489d
Marck C. Guzmán <marck@marck-devs.com>
2022-01-12 15:10:35 +0100

    Fix(formatter): prevent error on empty data

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,9 @@ export class Formatter {
   public static formatObj(line: string, data: any): string {
     let keys = Object.keys(data);
     let out = line;
+    if (!out) {
+      out = "";
+    }
     for (let k of keys) {
       out = out.replace(new RegExp("\\{" + k + "\\}", "gi"), data[k]);
     }
@@ -15,6 +18,9 @@ export class Formatter {
   public static format(line: string, ...args): string {
     let keys = Object.keys(args);
     let out = line;
+    if (!out) {
+      out = "";
+    }
     for (let k in keys) {
       out = out.replace(new RegExp("\\{" + k + "\\}", "gi"), args[k]);
     }
